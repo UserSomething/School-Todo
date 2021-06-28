e2124db77ee3a111f38af36532fff6ac7611ad67
UserSomething <34489820+UserSomething@users.noreply.github.com>
2021-06-28 16:26:36 +0200

    Add project files.

---
+++ School-Todo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using School_Todo;
+
+namespace School_Todo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

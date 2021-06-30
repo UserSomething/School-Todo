6b3a661097177b446746ff540e787235e54abb35
UserSomething <34489820+UserSomething@users.noreply.github.com>
2021-06-30 20:00:36 +0200

    PersonSequencer class and its tests added.

---
+++ School-Todo.Tests/PersonSequencerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using School_Todo.Data;
+
+namespace TodoP.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void When_NextPersonIdMethodCalled_Expect_PersonIdEqualsOne()
+        {
+            int expected = 1;
+            int actual = PersonSeqencer.NextPersonId();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void When_ResetMethodCalled_Expect_PersonIdEqualsZero()
+        {
+            int expected = 0;
+            int idIsOne = PersonSeqencer.NextPersonId();
+
+            PersonSeqencer.Reset();
+            int actual = PersonSeqencer.PersonId;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ School-Todo/Data/PersonSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace School_Todo.Data
+{
+    public static class PersonSeqencer
+    {
+        private static int personId = 0;
+
+        public static int PersonId { get { return personId; } }
+
+        public static int NextPersonId()
+        {
+            return ++personId;
+        }
+
+        public static void Reset()
+        {
+            personId = 0;
+        }
+    }
+}
